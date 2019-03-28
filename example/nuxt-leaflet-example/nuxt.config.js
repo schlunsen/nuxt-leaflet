a880a1a29666c139ee11f674d1905c4f652a2566
Rasmus Schlünsen <rasmus@schlunsen.com>
2019-03-28 13:27:31 +0100

    Added example nuxt project

---
+++ example/nuxt-leaflet-example/nuxt.config.js
@@ -0,0 +1,70 @@
+import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~/assets/style/app.styl'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/vuetify'
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    ['nuxt-leaflet', { /* module options */ }],
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: ['vuetify/lib'],
+    plugins: [new VuetifyLoaderPlugin()],
+    loaders: {
+      stylus: {
+        import: ['~assets/style/variables.styl']
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ example/nuxt-leaflet-example/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div id="map-wrap" style="height: 100vh; width: 100%;">
+      <no-ssr>
+        <l-map :zoom="13" :center="[47.413220, -1.219482]">
+          <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+          <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker>
+        </l-map>
+      </no-ssr>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    
+  }
+};
+</script>
+
